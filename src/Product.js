cbefade291a85fe8f26ee789ef27e0d2a370b0d7
Mohamed Ahmed <a.mohamed.cali@gmail.com>
2020-09-09 09:53:43 +0200

    feat: create Home page

---
+++ src/Product.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import './Product.css'
+
+
+function Product({ title, image, price, rating }) {
+    return (
+        <div className='product'>
+            <div className="product__info">
+                <p>{title}</p>
+                <p className='product__price'>
+                    <small>$</small>
+                    <strong>{price}</strong>
+                </p>
+
+                <div className='product_rating'>
+                    {Array(rating).fill().map((_, i) => (
+                        <p>🌟</p>  
+                    ))}
+                </div>
+            </div>
+
+            <img
+                src={image}
+            />
+
+            <button>Add to Basket</button>
+        </div>
+    )
+}
+
+export default Product
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBWrVVcS0y56R3qcLrMkuwTtJS2lxGbwsY",
+    authDomain: "clone-7d2a4.firebaseapp.com",
+    databaseURL: "https://clone-7d2a4.firebaseio.com",
+    projectId: "clone-7d2a4",
+    storageBucket: "clone-7d2a4.appspot.com",
+    messagingSenderId: "266719854578",
+    appId: "1:266719854578:web:d3b87df19b9c3b441c356b",
+    measurementId: "G-2FTFBJ9S4X"
+};
