204841139f2a79961c14a2cba1fbc3e4d0b970c9
Mohamed Ahmed <a.mohamed.cali@gmail.com>
2020-09-11 17:18:16 +0200

    feat: add Payment page

---
+++ src/axios.js
@@ -0,0 +1,8 @@
+import axios from 'axios'
+
+
+const instance = axios.create({
+    baseURL: 'http://localhost:5001/clone-7d2a4/us-central1/api' // The API (cloud function) URL
+})
+
+export default instance
