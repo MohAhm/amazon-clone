716fdca7600a3d40e94d70c2df41cef595f11eb6
Mohamed Ahmed <a.mohamed.cali@gmail.com>
2020-09-13 11:58:36 +0200

    docs: update Readme

--- src/Home.js
+++ src/Home.js
@@ -15,6 +15,23 @@ function Home() {
 
                 <div className="home__row">
                     <Product
+                        id="3254354345"
+                        title="New Apple iPad Pro (12.9-inch, Wi-Fi, 128GB) - Silver (4th Generation)"
+                        price={598.99}
+                        rating={4}
+                        image="https://images-na.ssl-images-amazon.com/images/I/816ctt5WV5L._AC_SX385_.jpg"
+                    />
+                    <Product
+                        id="23445930"
+                        title="Amazon Echo (3rd generation) | Smart speaker with Alexa, Charcoal Fabric"
+                        price={98.99}
+                        rating={5}
+                        image="https://media.very.co.uk/i/very/P6LTG_SQ1_0000000071_CHARCOAL_SLf?$300x400_retinamobilex2$"
+                    />
+                </div>
+
+                <div className="home__row">
+                    <Product
                         id="12321341"
                         title="The lean startup: How Constant Innovation Creates Radically Successful Businesses Paperback"
                         price={29.99}
@@ -28,29 +45,12 @@ function Home() {
                         rating={4}
                         image="https://images-na.ssl-images-amazon.com/images/I/91gRKbX%2BS8L._AC_SL1500_.jpg"
                     />
-                </div>
-
-                <div className="home__row">
                     <Product
                         id="4903850"
                         title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor"
                         price={199.99}
                         rating={3}
                         image="https://images-na.ssl-images-amazon.com/images/I/71Swqqe7XAL._AC_SX466_.jpg"
-                    />
-                    <Product
-                        id="23445930"
-                        title="Amazon Echo (3rd generation) | Smart speaker with Alexa, Charcoal Fabric"
-                        price={98.99}
-                        rating={5}
-                        image="https://media.very.co.uk/i/very/P6LTG_SQ1_0000000071_CHARCOAL_SLf?$300x400_retinamobilex2$"
-                    />
-                    <Product
-                        id="3254354345"
-                        title="New Apple iPad Pro (12.9-inch, Wi-Fi, 128GB) - Silver (4th Generation)"
-                        price={598.99}
-                        rating={4}
-                        image="https://images-na.ssl-images-amazon.com/images/I/816ctt5WV5L._AC_SX385_.jpg"
                     />
                 </div>
 
